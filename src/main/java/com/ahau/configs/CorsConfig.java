896f0ccee9aad7377f6656ab85167591a7ffe446
daydaywritecode <1172032260@qq.com>
2018-11-09 19:22:02 +0800

    new CorsConfig

---
+++ src/main/java/com/ahau/configs/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.ahau.configs;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+
+    //设置跨域访问
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
+                .maxAge(3600);
+    }
+}
