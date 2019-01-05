99932d6fe925b308be1aeba1ddabb2ed516d20b6
daydaywritecode <1172032260@qq.com>
2019-01-05 22:29:21 +0800

    modify UserController

--- src/main/java/com/ahau/controller/System/UserController.java
+++ src/main/java/com/ahau/controller/System/UserController.java
@@ -158,21 +158,4 @@ public class UserController {
         }
     }
 
-
-//    @ApiOperation("生成验证码")
-//    @GetMapping("/getcode")
-//    public void getCode(HttpServletResponse response, HttpServletRequest request) throws Exception{
-//        HttpSession session=request.getSession();
-//        //利用图片工具生成图片
-//        //第一个参数是生成的验证码，第二个参数是生成的图片
-//        Object[] objs = VerifyUtil.createImage();
-//        //将验证码存入Session
-//        session.setAttribute("imageCode",objs[0]);
-//
-//        //将图片输出给浏览器
-//        BufferedImage image = (BufferedImage) objs[1];
-//        response.setContentType("image/png");
-//        OutputStream os = response.getOutputStream();
-//        ImageIO.write(image, "png", os);
-//    }
 }
