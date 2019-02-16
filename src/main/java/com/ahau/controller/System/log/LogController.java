0a4711a65f3c8b54cfb65b9a091ad866465d1139
daydaywritecode <1172032260@qq.com>
2019-02-16 15:04:50 +0800

    add Log API

--- src/main/java/com/ahau/controller/System/log/LogController.java
+++ src/main/java/com/ahau/controller/System/log/LogController.java
@@ -1,4 +1,38 @@
 package com.ahau.controller.System.log;
 
+import com.ahau.BambootattanServerApplication;
+import com.ahau.entity.bamboo.base.Result;
+import com.ahau.service.system.log.LogService;
+import com.ahau.utils.ResultUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 描述：日志控制层接口
+ * Created by: zhangli
+ * 2019-2-16
+ */
+@RestController
+@Api(description = "日志接口")
+@RequestMapping(value = "/log")
 public class LogController {
+    private final LogService logService;
+    private static final Logger LOGGER = LogManager.getLogger(BambootattanServerApplication.class);
+
+    @Autowired
+    public LogController(LogService logService) {this.logService = logService;}
+
+    /**
+     * 查询所有日志
+     * @return
+     */
+    @ApiOperation(value = "获取所有日志列表", notes = "获取所有日志列表")
+    @GetMapping("findAll")
+    public Result findAll() { return ResultUtil.success(logService.findAll()); }
 }
--- src/main/java/com/ahau/service/system/log/LogService.java
+++ src/main/java/com/ahau/service/system/log/LogService.java
@@ -1,4 +1,28 @@
 package com.ahau.service.system.log;
 
+import com.ahau.BambootattanServerApplication;
+import com.ahau.entity.system.log.Log;
+import com.ahau.repository.system.log.LogRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * 描述：日志服务层接口
+ * Created by: zhangli
+ * 2019-2-16
+ */
+@Transactional
+@Service
 public class LogService {
+    private final LogRepository logRepository;
+    private static final Logger LOGGER = LogManager.getLogger(BambootattanServerApplication.class);
+
+    @Autowired
+    public LogService(LogRepository logRepository) {this.logRepository = logRepository;}
+
+    public List<Log> findAll() { return logRepository.findAll();}
 }
