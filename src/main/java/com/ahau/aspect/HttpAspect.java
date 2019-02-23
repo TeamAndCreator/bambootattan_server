d8e5f312edc6bf6bca27edb9d75aa9b725103a89
liulianjushi <liulianjushi@126.com>
2019-02-23 20:01:48 +0800

    add log save database

--- src/main/java/com/ahau/aspect/HttpAspect.java
+++ src/main/java/com/ahau/aspect/HttpAspect.java
@@ -1,11 +1,14 @@
 package com.ahau.aspect;
 
 
+import com.ahau.entity.system.log.Log;
+import com.ahau.service.system.log.LogService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -15,7 +18,12 @@ import javax.servlet.http.HttpServletRequest;
 @Aspect
 @Component
 public class HttpAspect {
+
+    private LogService logService;
+
     private static final Logger LOGGER = LogManager.getLogger(HttpAspect.class);
+
+
 
     @Pointcut("execution(public * com.ahau.controller.*.*.*.*(..))")
     public void log() {
@@ -28,9 +36,17 @@ public class HttpAspect {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         assert attributes != null;
         HttpServletRequest request = attributes.getRequest();
-        LOGGER.info("name={},url={},method={},ip={},class_method={},args={}",request.getSession().getAttribute("username"), request.getRequestURI(),request.getMethod(),
-                request.getRemoteAddr(), joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName()
-                , joinPoint.getArgs());
+        Log log=new Log();
+        log.setOptUrl(request.getRequestURI());
+//        log.setOptUser(request.getSession().getAttribute("username"));
+//        log.setClassMethod(joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+//        log.setOptIp(request.getRemoteAddr());
+        LOGGER.info(log.toString());
+//        log.setRequestParam(joinPoint.getArgs().toString());
+        logService.save(log);
+//        LOGGER.info("name={},url={},method={},ip={},class_method={},args={}",request.getSession().getAttribute("username"), request.getRequestURI(),request.getMethod(),
+//                request.getRemoteAddr(), joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName()
+//                , joinPoint.getArgs());
     }
 
     @AfterReturning(returning = "object",pointcut = "log()")
