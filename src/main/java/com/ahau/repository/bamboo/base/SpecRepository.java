a5cc1d7030254590fd538d279bd09c7fdfe632d7
daydaywritecode <1172032260@qq.com>
2019-03-26 20:33:09 +0800

    add findAllQueryWithOutUnderstem

--- src/main/java/com/ahau/repository/bamboo/base/SpecRepository.java
+++ src/main/java/com/ahau/repository/bamboo/base/SpecRepository.java
@@ -1,7 +1,9 @@
 package com.ahau.repository.bamboo.base;
 
 import com.ahau.entity.bamboo.base.Spec;
-import com.ahau.entity.file.Files;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
@@ -9,7 +11,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Set;
 
 /**
  * 竹种Repository
@@ -25,10 +26,24 @@ public interface SpecRepository extends JpaRepository<Spec, Long>, JpaSpecificat
     /**
      * 根据spec的id查找其files
      * 用于更新文件
+     * @param id
+     * @return
      */
     @Query(value = "select s.files from Spec as s where s.id=:id")
     @Modifying
     List getFiles(@Param(value = "id") Long id);
 
 //    Set<Files> getFiles(Long id);
+
+    /**
+     * 分页有条件查找（不包含已有地下茎的数据）
+     * @param specification
+     * @param pageable
+     * @return
+     */
+    @Query(value = "SELECT A FROM Spec AS A WHERE NOT EXISTS( SELECT B.spec FROM Understem AS B WHERE A.specId=B.spec)")
+    Page<Spec> findAllQueryWithOutUnderstem(Specification<Spec> specification, Pageable pageable);
+
+    @Query(value = "SELECT A FROM Spec AS A WHERE NOT EXISTS( SELECT B.spec FROM Understem AS B WHERE A.specId=B.spec)")
+    Page<Spec> findAllQueryWithOutUnderstem(Pageable pageable);
 }
