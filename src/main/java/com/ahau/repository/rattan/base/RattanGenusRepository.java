52b18f0ea5d7b6fdfe49df17a4dc5a2ecc2fa79e
daydaywritecode <1172032260@qq.com>
2019-04-04 21:21:02 +0800

    update RattanGenus API

--- src/main/java/com/ahau/repository/rattan/base/RattanGenusRepository.java
+++ src/main/java/com/ahau/repository/rattan/base/RattanGenusRepository.java
@@ -3,6 +3,7 @@ package com.ahau.repository.rattan.base;
 import com.ahau.entity.rattan.base.RattanGenus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -15,4 +16,9 @@ public interface RattanGenusRepository extends JpaRepository<RattanGenus, Long>,
 
     void deleteByGenusIdIn(List<Long> ids);
     void findByGenusIdIn(List<Long> ids);
+
+    @Query(value = "SELECT COUNT(genusId) FROM RattanGenus WHERE genusNameCh=?1")
+    Integer isNameChExisted(String nameCh);
+    @Query(value = "SELECT COUNT(genusId) FROM RattanGenus WHERE genusNameCh=?1 AND genusId<>?2")
+    Integer isNameChExisted(String nameCh,Long genusId);
 }
